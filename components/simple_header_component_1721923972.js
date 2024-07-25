91c61b36100483acea836dd86aef2d80bc362db7
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-25 21:43:36 +0530

    Code generation

--- components/simple_header_component_1721923972.js
+++ components/simple_header_component_1721923972.js
@@ -28,7 +28,7 @@ Vue.component("simple_header_component_1721923972", {
                         <li id="nav-item-2"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Notifications for new transactions</a></li>
                         <li id="nav-item-3"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Integration with wallet apps</a></li>
                     </ul>
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded shadow-md">
                         Hello World
                     </button>
                 </div>
